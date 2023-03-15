d2407cb25e447da5a8788cbdf10d1d78f8e1b339
Lior Gabay <lior.gabay@888holdings.com>
2023-03-15 21:02:17 +0200

    Partially Completed Banking Microservice

--- MicroRabbit/MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Program.cs
@@ -1,11 +1,38 @@
+using MediatR;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddDbContext<BankingDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("BankingDbConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Banking Microservice", Version = "v1" });
+});
+
+builder.Services.AddMediatR(typeof(StartupBase));
+
+RegisterServices(builder.Services);
+
+void RegisterServices(IServiceCollection services)
+{
+    DependencyContainer.RegisterServices(services);
+}
 
 var app = builder.Build();
 
@@ -17,6 +44,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice V1");
+});
 
 app.UseAuthorization();
 
---
+++ MicroRabbit/MicroRabbit.Banking.Data/Context/BankingDbContext.cs
@@ -0,0 +1,32 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    public class BankingDbContext : DbContext
+    {
+        public BankingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+
+
+    public class BankingDbContextFactory : IDesignTimeDbContextFactory<BankingDbContext>
+    {
+        public BankingDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BankingDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;DataBase=BankingDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+
+            return new BankingDbContext(optionsBuilder.Options);
+        }
+    }
+}

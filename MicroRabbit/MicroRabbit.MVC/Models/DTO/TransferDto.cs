d49ff610f3954fd82d4aba1d37d0f1c941664e2d
Lior Gabay <lior.gabay@888holdings.com>
2023-03-19 16:24:15 +0200

    Initial MicroRabbit MVC

---
+++ MicroRabbit/MicroRabbit.MVC/Models/DTO/TransferDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MicroRabbit.MVC.Models.DTO
+{
+    public class TransferDto
+    {
+        public int FromType { get; set; }
+
+        public int ToType { get; set; }
+
+        public decimal TransferAmount { get; set; }
+    }
+}
